1b947c3623aac49a257cb2a58690dd2a1fce14e1
swati259216 <swati@zemosolabs.com>
2014-12-05 18:06:26 +0530

    ssss

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -8,7 +8,10 @@ import views.html.*;
 public class Application extends Controller {
 
     public static Result index() {
+        System.out.print("hello");
         return ok(index.render("Your new application is ready."));
+
+
     }
 
 }
